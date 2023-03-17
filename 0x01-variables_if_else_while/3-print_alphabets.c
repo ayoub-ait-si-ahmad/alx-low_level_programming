19f3599eb157ba8a671b0c355d6bb903b7ee7113
ayoub <ayoub.ait.si.ahmad.86@gmail.com>
2023-03-17 06:50:17 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		x = tolower(x);
+		putchar(x);
+		x++;
+	}
+	 int w = 'A';
+
+		 while (w <= 'Z')
+	 {
+		w = tolower(w);
+		putchar(w);
+		w++;
+	 }
+	 putchar('\n');
+	 return (0);
+}
