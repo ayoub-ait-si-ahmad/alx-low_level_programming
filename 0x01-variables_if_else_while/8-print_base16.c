5211b1325be17e06569228a3ff55dc693076f973
ayoub <ayoub.ait.si.ahmad.86@gmail.com>
2023-03-17 09:08:07 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x = '0';
+	int y = 'a';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+		while (y <= 'f')
+		{
+			putchar(y);
+			y++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
